094e3ae420d3ff0a19b242508fd5efdd0e3856ba
Floreaviulian <florea.viulian@gmail.com>
2023-12-13 07:51:48 +0200

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by FloreaIulian on 12/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
